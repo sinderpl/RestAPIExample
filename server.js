2a53190ab68b0d878492eff88ad9ca0b8511afd1
sinderpl <sinderpl@gmail.com>
2017-05-24 14:47:29 +0100

    Basic Rest API created, all types of requests can now be called.

--- server.js
+++ server.js
@@ -6,28 +6,40 @@
 //Require the Express resource for handling NodeJS API
 const express = require('express');
 
-//Define a expression application
+//Define a express application
 var app = express();
 
+
+    //Methods defined
 //Base get request for the homepage
-//Can be called with curl localhost:3000
-app.get('/', function (req, res){
+//Can be called with ' curl localhost:3000 '
+app.get('/get', function (req, res){
   res.send('Get called.');
 });
 
 //Post request for the homepage
-app.post('/' , function (req, res){
+//Can be called with ' curl localhost:3000/post '
+app.post('/post' , function (req, res){
   res.send('Post called.');
 });
 
 //Put request for the homepage
-app.put('/', function(req, res){
+//Can be called with ' curl localhost:3000/put '
+app.put('/put', function(req, res){
   res.send("Put called");
 });
 
 //Delete request for the homepage
-app.delete('/', function(req, res){
+//Can be called with ' curl localhost:3000/delete '
+app.delete('/delete', function(req, res){
   res.send("Delete called");
+});
+
+//The following code allows for input from all the HTTP requests,
+// it is a more universal type of method.
+//Can be called with ' curl localhost:3000/all '
+app.all('/all', function(req, res){
+  res.send('All function called.');
 });
 
 //Start listening for requests to the API on port 3030
