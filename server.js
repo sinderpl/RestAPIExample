dd4bf25c06f3cd4133e3b8c1b12a6288360880b4
sinderpl <sinderpl@gmail.com>
2017-05-24 14:37:20 +0100

    Now utilising all four basic CRUD HTTP commands

--- server.js
+++ server.js
@@ -10,8 +10,24 @@ const express = require('express');
 var app = express();
 
 //Base get request for the homepage
+//Can be called with curl localhost:3000
 app.get('/', function (req, res){
-  res.send('Hello World!');
+  res.send('Get called.');
+});
+
+//Post request for the homepage
+app.post('/' , function (req, res){
+  res.send('Post called.');
+});
+
+//Put request for the homepage
+app.put('/', function(req, res){
+  res.send("Put called");
+});
+
+//Delete request for the homepage
+app.delete('/', function(req, res){
+  res.send("Delete called");
 });
 
 //Start listening for requests to the API on port 3030
